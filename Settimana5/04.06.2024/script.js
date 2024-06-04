f2290eaf4decbdae46b6a9fcd6995b558e6c92b7
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-06-04 16:25:04 +0200

    add comment

--- Settimana5/04.06.2024/script.js
+++ Settimana5/04.06.2024/script.js
@@ -2,7 +2,7 @@ let intervalId;
 
 function updateClock() {
     const now = new Date();
-    console.log(now);
+    console.log(now); // Print the current date to the console
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
