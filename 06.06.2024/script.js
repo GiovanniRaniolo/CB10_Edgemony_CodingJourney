02a8b34f80d868634b355a6e7d014f2308046ea6
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-06-06 13:54:57 +0200

    Exercise 1 - Completed

---
+++ 06.06.2024/script.js
@@ -0,0 +1,6 @@
+fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Errore:', error));
