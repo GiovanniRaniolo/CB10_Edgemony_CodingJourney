8f6f2cc2620fbeb753a317db90aa1d1d31d31ed8
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-07-04 20:03:16 +0200

    feat: add exercise 04.07.2024 Mortgage Calculator

---
+++ 04.07.2024/mortgage-calculator/src/App.jsx
@@ -0,0 +1,173 @@
+import { useState } from "react";
+import style from "./App.module.css";
+import calculateIcon from "./assets/icon-calculator.svg";
+import illustration from "./assets/illustration-empty.svg";
+
+function App() {
+  const initialMortgageState = {
+    mortgageAmount: "",
+    mortgageTerm: "",
+    interestRate: "",
+    paymentType: "repayment",
+  };
+
+  const [mortgageData, setMortgageData] = useState({ ...initialMortgageState });
+  const [showResults, setShowResults] = useState(false);
+  const [monthlyPayment, setMonthlyPayment] = useState("");
+  const [totalRepay, setTotalRepay] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setMortgageData({
+      ...mortgageData,
+      [name]: value,
+    });
+  };
+
+  const calculateMonthlyRepayments = () => {
+    const { mortgageAmount, mortgageTerm, interestRate, paymentType } =
+      mortgageData;
+
+    const amount = parseFloat(mortgageAmount);
+    const term = parseFloat(mortgageTerm);
+    const rate = parseFloat(interestRate) / 100;
+
+    let monthlyPayment = 0;
+    if (paymentType === "repayment") {
+      monthlyPayment =
+        (amount * rate) / (12 * (1 - Math.pow(1 + rate / 12, -term)));
+    } else {
+      monthlyPayment = (amount * rate) / 12;
+    }
+
+    const totalRepay = monthlyPayment * term;
+
+    return {
+      monthlyPayment: monthlyPayment.toFixed(2),
+      totalRepay: totalRepay.toFixed(2),
+    };
+  };
+
+  const handleCalculate = () => {
+    const results = calculateMonthlyRepayments();
+    setShowResults(true);
+    setMonthlyPayment(results.monthlyPayment);
+    setTotalRepay(results.totalRepay);
+  };
+
+  const handleClearAll = () => {
+    setMortgageData({ ...initialMortgageState });
+    setShowResults(false);
+    setMonthlyPayment("");
+    setTotalRepay("");
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.calculator}>
+        <div className={style.title}>
+          <h1>Mortgage Calculator</h1>
+          <span className={style.clear} onClick={handleClearAll}>
+            Clear All
+          </span>
+        </div>
+        <div className={style.inputGroup}>
+          <label htmlFor="amount">Mortgage Amount</label>
+          <div className={style.inputWithIcon}>
+            <span className={style.spanAmount}>£</span>
+            <input
+              type="number"
+              name="mortgageAmount"
+              value={mortgageData.mortgageAmount}
+              onChange={handleInputChange}
+              className={style.amount}
+            />
+          </div>
+          <div className={style.yearsPercent}>
+            <div className={style.inputGroup}>
+              <label htmlFor="years">Mortgage Term (years)</label>
+              <input
+                type="number"
+                name="mortgageTerm"
+                value={mortgageData.mortgageTerm}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className={style.inputGroup}>
+              <label htmlFor="percent">Interest Rate (%)</label>
+              <input
+                type="number"
+                name="interestRate"
+                value={mortgageData.interestRate}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div className={style.radioButton}>
+            <label>Mortgage Type</label>
+            <div className={style.radioContainer}>
+              <label>
+                <input
+                  type="radio"
+                  name="paymentType"
+                  value="repayment"
+                  checked={mortgageData.paymentType === "repayment"}
+                  onChange={handleInputChange}
+                />
+                Repayment
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="paymentType"
+                  value="interestOnly"
+                  checked={mortgageData.paymentType === "interestOnly"}
+                  onChange={handleInputChange}
+                />
+                Interest Only
+              </label>
+            </div>
+          </div>
+          <button className={style.calculateButton} onClick={handleCalculate}>
+            <img
+              src={calculateIcon}
+              alt="Calculate Icon"
+              className={style.buttonIcon}
+            />
+            Calculate Repayments
+          </button>
+        </div>
+      </div>
+      <div className={`${style.results} ${showResults ? style.show : ""}`}>
+        {!showResults && (
+          <>
+            <img src={illustration} alt="" />
+            <h2>Results shown here</h2>
+            <p>
+              Complete the form and click "calculate repayments to see what your
+              monthly repayments would be
+            </p>
+          </>
+        )}
+        {showResults && (
+          <>
+            <h2>Your results</h2>
+            <p>
+              Your results are shown below based on the information you
+              provided.
+            </p>
+            <div className={style.resultSection}>
+              <h3>Your monthly repayments</h3>
+              <p className={style.resultAmount}>£{monthlyPayment}</p>
+              <hr />
+              <p>Total you'll repay over the term</p>
+              <p className={style.resultTotal}>£{totalRepay}</p>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
